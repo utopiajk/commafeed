3d96e8c2f5979ab2ac9922363d6ef4e15b8f6220
Athou <jeremiepanzer@gmail.com>
2013-06-07 09:59:29 +0200

    lock on sorted list of entries

--- src/main/java/com/commafeed/backend/feeds/FeedRefreshUpdater.java
+++ src/main/java/com/commafeed/backend/feeds/FeedRefreshUpdater.java
@@ -2,18 +2,23 @@ package com.commafeed.backend.feeds;
 
 import java.util.Calendar;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
+import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.RejectedExecutionHandler;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang3.time.DateUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,11 +26,14 @@ import org.slf4j.LoggerFactory;
 import com.commafeed.backend.MetricsBean;
 import com.commafeed.backend.dao.FeedDAO;
 import com.commafeed.backend.dao.FeedEntryDAO;
+import com.commafeed.backend.model.ApplicationSettings;
 import com.commafeed.backend.model.Feed;
 import com.commafeed.backend.model.FeedEntry;
 import com.commafeed.backend.pubsubhubbub.SubscriptionHandler;
 import com.commafeed.backend.services.ApplicationSettingsService;
 import com.commafeed.backend.services.FeedUpdateService;
+import com.google.api.client.util.Lists;
+import com.google.common.util.concurrent.Striped;
 
 @Singleton
 public class FeedRefreshUpdater {
@@ -56,11 +64,17 @@ public class FeedRefreshUpdater {
 
 	private ThreadPoolExecutor pool;
 	private BlockingQueue<Runnable> queue;
+	private Striped<Lock> locks;
 
 	@PostConstruct
 	public void init() {
-		pool = new ThreadPoolExecutor(1, 1, 0, TimeUnit.MILLISECONDS,
-				queue = new ArrayBlockingQueue<Runnable>(10000));
+		ApplicationSettings settings = applicationSettingsService.get();
+		int threads = Math.max(settings.getDatabaseUpdateThreads(), 1);
+		log.info("Creating database pool with {} threads", threads);
+		locks = Striped.lazyWeakLock(threads * 1000);
+		pool = new ThreadPoolExecutor(threads, threads, 0,
+				TimeUnit.MILLISECONDS,
+				queue = new ArrayBlockingQueue<Runnable>(500 * threads));
 		pool.setRejectedExecutionHandler(new RejectedExecutionHandler() {
 			@Override
 			public void rejectedExecution(Runnable r, ThreadPoolExecutor e) {
@@ -106,7 +120,7 @@ public class FeedRefreshUpdater {
 			if (entries.isEmpty() == false) {
 
 				try {
-					feedUpdateService.updateEntries(feed, entries);
+					updateEntries(feed, entries);
 				} catch (Exception e) {
 					log.error(e.getMessage(), e);
 					ok = false;
@@ -122,6 +136,60 @@ public class FeedRefreshUpdater {
 			metricsBean.feedUpdated();
 			taskGiver.giveBack(feed);
 		}
+	}
+
+	private void updateEntries(Feed feed, Collection<FeedEntry> entries)
+			throws InterruptedException {
+		entries = sortEntries(entries);
+
+		List<Lock> currentLocks = Lists.newArrayList();
+		for (FeedEntry entry : entries) {
+			String key = getKey(entry);
+			Lock lock = locks.get(key);
+
+			try {
+				boolean locked = lock.tryLock(1, TimeUnit.MINUTES);
+				if (locked) {
+					currentLocks.add(lock);
+				} else {
+					throw new InterruptedException("lock timeout for "
+							+ feed.getUrl() + " - " + key);
+				}
+			} catch (InterruptedException e) {
+				for (Lock l : currentLocks) {
+					l.unlock();
+				}
+				throw e;
+			}
+		}
+
+		try {
+			feedUpdateService.updateEntries(feed, entries);
+		} finally {
+			for (Lock l : currentLocks) {
+				l.unlock();
+			}
+		}
+	}
+
+	private List<FeedEntry> sortEntries(Collection<FeedEntry> entries) {
+		List<FeedEntry> list = Lists.newArrayList();
+		for (FeedEntry entry : entries) {
+			list.add(entry);
+		}
+		Collections.sort(list, new Comparator<FeedEntry>() {
+			@Override
+			public int compare(FeedEntry o1, FeedEntry o2) {
+				String k1 = getKey(o1);
+				String k2 = getKey(o2);
+				return k1.compareTo(k2);
+			}
+		});
+		return list;
+	}
+
+	private String getKey(FeedEntry entry) {
+		return StringUtils.trimToEmpty(entry.getGuid() + entry.getUrl());
 	}
 
 	private void handlePubSub(final Feed feed) {
