14432d98833834734a83dde3c52551023c556ab5
Athou <jeremiepanzer@gmail.com>
2013-06-07 13:57:00 +0200

    reduce timer

--- src/main/java/com/commafeed/backend/pubsubhubbub/SubscriptionHandler.java
+++ src/main/java/com/commafeed/backend/pubsubhubbub/SubscriptionHandler.java
@@ -37,6 +37,13 @@ public class SubscriptionHandler {
 
 	public void subscribe(Feed feed) {
 
+		try {
+			// waiting before subscribing to pubsub
+			Thread.sleep(15000);
+		} catch (InterruptedException e1) {
+			// do nothing
+		}
+
 		String hub = feed.getPushHub();
 		String topic = feed.getPushTopic();
 		String publicUrl = FeedUtils
